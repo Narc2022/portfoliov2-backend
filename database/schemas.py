3ff7c1b1e75e58ef6f4eb0a8f8257d4255d0496e
sachin <sachinnarofficials@gmail.com>
2025-11-16 20:11:07 +0530

    schema comment added

--- database/schemas.py
+++ database/schemas.py
@@ -1,3 +1,4 @@
+# CREATE Dictionary
 def individual_data(todo):
     return {
         "id": str(todo["_id"]), #as mongodb store data in object so here we typcaste id to string
@@ -5,7 +6,8 @@ def individual_data(todo):
         "description": todo["description"],
         "status": todo["is_completed"]
     }
-    
-#create list of dictionary
+
+
+#CREATE LIST OF Dictionary
 def all_tasks(todos):
     return [individual_data(todo) for todo in todos]
