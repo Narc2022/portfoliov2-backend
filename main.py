deb63feca75a39241c64975ca7887a60184e42e6
sachin <sachinnarofficials@gmail.com>
2025-11-16 20:52:19 +0530

    get todo list api created route /v1/api

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI,APIRouter
+from fastapi import FastAPI, APIRouter
+from configrations import collection
+from database.schemas import all_tasks
 
 app = FastAPI()
 router = APIRouter()
+
+@router.get("/v1/api")
+async def get_all_todos():
+    data = collection.find()
+    return all_tasks(data)
 
 
 app.include_router(router)
