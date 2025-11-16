f8c9e8a466177616e10da6ad3ce48fa561d21073
sachin <sachinnarofficials@gmail.com>
2025-11-16 16:46:34 +0530

    configrations.py

---
+++ configrations.py
@@ -0,0 +1,10 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://eduCode:<db_password>@codeai.2ablb2s.mongodb.net/?appName=codeAi"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+db = client.codeaidb
+collection = db["codeaidb_data"]
