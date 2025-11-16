81cbbdcaf09dc95d7613ce152e6f1c562864dda5
sachin <sachinnarofficials@gmail.com>
2025-11-16 21:48:39 +0530

    post api created and configrations error fixed

--- configrations.py
+++ configrations.py
@@ -1,7 +1,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://eduCode:eduCode2026$$$@codeai.2ablb2s.mongodb.net/?appName=codeAi"
+uri = "mongodb+srv://eduCode:eduCodeAI243$$$@codeai.2ablb2s.mongodb.net/?appName=codeAi"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, HTTPException
 from configrations import collection
 from database.schemas import all_tasks
+from database.models import Todo
 
 app = FastAPI()
 router = APIRouter()
@@ -10,5 +11,12 @@ async def get_all_todos():
     data = collection.find()
     return all_tasks(data)
 
-
+@router.post("/v1/api")
+async def create_task(new_task:Todo):
+    try:
+        resp = collection.insert_one(dict(new_task))
+        return {"status_code":200, "id":str(resp.inserted_id)}
+    except Exception as e:
+        return HTTPException(status_code=500, detail=f"Some error occured {e}")
+        
 app.include_router(router)
